bc64ca09ffda1e7fe6c053fe29b7c0aa12407d48
KunjPatel25 <kunj@ad.unc.edu>
2022-02-28 23:31:32 -0500

    ex06 finished

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -2,4 +2,41 @@
 
 __author__ = "730477806"
 
-def invert(a: dict[str, str]) -> dict[str, str]
+
+def invert(a: dict[str, str]) -> dict[str, str]:
+    """Invert the key and value items in the dictionary."""
+    invert_result: dict[str, str] = {}
+    for key in a:
+        value: str = a[key]
+        if value in invert_result:
+            raise KeyError
+        else:
+            invert_result[value] = key
+    return invert_result
+
+
+def favorite_color(b: dict[str, str]) -> str:
+    """Returning the color that appears most frequesntly."""
+    color_result: dict[str, int] = {}
+    max: int = 0
+    color: str = ""
+    for key in b:
+        if b[key] in color_result:
+            color_result[b[key]] += 1
+        else:
+            color_result[b[key]] = 1
+    for key in color_result:
+        if color_result[key] > max:
+            max = color_result[key]
+            color = key
+    return color
+
+
+def count(b: list[str]) -> dict[str, int]:
+    count_result: dict[str, int] = {}
+    for key in b:
+        if key in count_result:
+            count_result[key] += 1
+        else:
+            count_result[key] = 1
+    return count_result
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -2,3 +2,58 @@
 
 __author__ = "730477806"
 
+from dictionary import invert, favorite_color, count
+
+
+def test_invert_edge() -> None:
+    """Testing an edge case for invert."""
+    invert_result: dict[str, str] = {}
+    assert invert(invert_result) == {}
+
+
+def test_invert_use1() -> None:
+    """Testing a use case for invert."""
+    invert_result: dict[str, str] = {"Kunj": "Patel", "Josh": "Tu"}
+    assert invert(invert_result) == {"Patel": "Kunj", "Tu": "Josh"}
+
+
+def test_invert_use2() -> None:
+    """Testing another use case for invert."""
+    invert_result: dict[str, str] = {"Kunj": "Blue", "Kris": "Green"}
+    assert invert(invert_result) == {"Blue": "Kunj", "Green": "Kris"}
+
+
+def test_favorite_color_edge() -> None:
+    """Testing an edge case for favorite color."""
+    color_result: dict[str, str] = {}
+    assert favorite_color(color_result) == ""
+
+
+def test_favorite_color_use1() -> None:
+    """Testing a use case for favorite color."""
+    color_result: dict[str, str] = {"Kunj": "Blue", "Josh": "Blue", "Kris": "Green"}
+    assert favorite_color(color_result) == "Blue"
+
+
+def test_favorite_color_use2() -> None:
+    """Testing another use case for favorite color."""
+    color_result: dict[str, str] = {"Kunj": "Green", "Josh": "Green", "Kris": "Blue", "Alex": "Blue"}
+    assert favorite_color(color_result) == "Green"
+
+
+def test_count_edge() -> None:
+    """Test an edge case for count."""
+    count_result: list[str] = []
+    assert count(count_result) == {}
+
+
+def test_count_use1() -> None:
+    """Test a use case for count."""
+    count_result: list[str] = ["Kunj", "Kunj", "Kris", "Josh"]
+    assert count(count_result) == {"Kunj": 2, "Kris": 1, "Josh": 1}
+
+
+def test_count_use2() -> None:
+    """Test another use case for count."""
+    count_result: list[str] = ["Kunj", "Kunj", "Kunj", "Kunj"]
+    assert count(count_result) == {"Kunj": 4}
