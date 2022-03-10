97e6233c4b8ad358800bb5a291d4d1b09cd7d942
KunjPatel25 <kunj@ad.unc.edu>
2022-03-10 12:59:08 -0500

    3/10 updates

--- lessons/practice.py
+++ lessons/practice.py
@@ -1,3 +1,8 @@
-def remove_ws(word: str) -> str:
-    """Remove white space."""
-    return word
+def repeat(word: str, num: int) -> str:
+    """Doc"""
+    i: int = 0
+    result: str = ""
+    while i <= num:
+        result = result + word[0]
+        i = i + 1
+    return result
